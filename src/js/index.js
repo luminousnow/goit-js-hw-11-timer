4d003fb5288a1319d324eb698f8a17de5734b996
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-10-26 23:57:20 +0200

    імпортує функцію Таймера, створює об'єкт

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../scss/style.scss';
+import CountdownTimer from './countdown-timer.js';
+
+const timer = new CountdownTimer({
+  selector: 'timer-1',
+  targetDate: new Date('Oct 27, 2020'),
+});
