46029432c8a054780f4ca86be6df52516a83e7ae
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-10-26 23:56:23 +0200

    виносить логіку таймера в окремий файл

---
+++ src/js/countdown-timer.js
@@ -0,0 +1,137 @@
+const refs = {
+  clockface: document.querySelector('.js-clockface'),
+};
+
+export default class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.intervalId = null;
+    this.selector = selector;
+    this.targetDate = targetDate;
+
+    this.start();
+    this.init();
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = this.targetDate - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+
+      if (time < 0) {
+        clearInterval(this.intervalId);
+        refs.clockface.innerHTML = `<div class="timer" id="${this.selector}">
+        <div class="field">
+          <span class="value" data-value="days">
+            00
+          </span>
+          <span class="label">Days</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="hours">
+            00
+          </span>
+          <span class="label">Hours</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="mins">
+            00
+          </span>
+          <span class="label">Minutes</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="secs">
+            00
+          </span>
+          <span class="label">Seconds</span>
+        </div>
+      </div>`;
+      }
+      this.updateClockface(time);
+    }, 1000);
+  }
+
+  /*
+   * - Принимает время в миллисекундах
+   * - Высчитывает сколько в них вмещается часов/минут/секунд
+   * - Рисует интерфейс
+   */
+  updateClockface({ days, hours, mins, secs }) {
+    refs.clockface.innerHTML = `<div class="timer" id="${this.selector}">
+        <div class="field">
+          <span class="value" data-value="days">
+            ${days}
+          </span>
+          <span class="label">Days</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="hours">
+            ${hours}
+          </span>
+          <span class="label">Hours</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="mins">
+            ${mins}
+          </span>
+          <span class="label">Minutes</span>
+        </div>
+
+        <div class="field">
+          <span class="value" data-value="secs">
+            ${secs}
+          </span>
+          <span class="label">Seconds</span>
+        </div>
+      </div>`;
+  }
+
+  /*
+   * - Принимает время в миллисекундах
+   * - Высчитывает сколько в них вмещается часов/минут/секунд
+   * - Возвращает обьект со свойствами hours, mins, secs
+   * - Адская копипаста со стека 💩
+   */
+  getTimeComponents(time) {
+    /*
+     * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+     * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+     */
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    /*
+     * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+     * остатка % и делим его на количество миллисекунд в одном часе
+     * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+     */
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+
+    /*
+     * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+     * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+     */
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    /*
+     * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+     * миллисекунд в одной секунде (1000)
+     */
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  /*
+   * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
+   */
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
